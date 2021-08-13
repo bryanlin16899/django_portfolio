1f8be20bbfa9ea29489585da817dd9c14539a98b
bryanlin <bryanlin16899@outlook.com>
2021-08-13 16:00:19 +0000

    Remove old stuff

--- portfolio_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Project, Project_study
-
-
-admin.site.register(Project)
-admin.site.register(Project_study)
--- portfolio_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Project, Project_study
-
-
-def home(request):
-    learn_notes = Project.objects.all()[0:15]
-    study_notes = Project_study.objects.all()[0:15]
-    return render(request, 'portfolio_app/home.html', {'learn_notes': learn_notes, 'study_notes': study_notes})
-
