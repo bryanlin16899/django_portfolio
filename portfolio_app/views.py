204ccd2dbe9b3a9586082374299f7903acc4db4e
Bryan Lin <bryanlin16899@outlook.com>
2021-08-13 20:18:40 +0800

    80% complete ready upload to github.

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Project_study
 
 
 def home(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio_app/home.html', {'projects': projects})
+    learn_notes = Project.objects.all()[0:15]
+    study_notes = Project_study.objects.all()[0:15]
+    return render(request, 'portfolio_app/home.html', {'learn_notes': learn_notes, 'study_notes': study_notes})
 
